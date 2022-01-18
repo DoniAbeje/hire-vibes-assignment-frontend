5414b64572951e1d5419ac330e86f32c6b666de1
Doni Abeje <doni@peoplise.com>
2022-01-18 22:13:14 +0300

    replace for and class attribs with htmlFor and className

--- pages/register.js
+++ pages/register.js
@@ -1,3 +1,5 @@
+import * as service from "../service";
+
 export default function Register() {
   return (
     <div
@@ -5,37 +7,37 @@ export default function Register() {
       className="d-flex justify-content-center align-items-center"
     >
       <form className="bg-light p-5 shadow-sm">
-        <div class="mb-3" style={{ width: "30rem" }}>
+        <div className="mb-3" style={{ width: "30rem" }}>
           <h1 className="mb-5 text-center">
             <span className="text-primary">FILMS</span> | Register
           </h1>
-          <label for="username-input" class="form-label">
+          <label htmlFor="username-input" className="form-label">
             Username
           </label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             id="username-input"
             placeholder=""
           />
         </div>
-        <div class="mb-3">
-          <label for="password-input" class="form-label">
+        <div className="mb-3">
+          <label htmlFor="password-input" className="form-label">
             Password
           </label>
-          <input type="password" class="form-control" id="password-input" />
+          <input type="password" className="form-control" id="password-input" />
         </div>
-        <div class="mb-3">
-          <label for="confirm-password-input" class="form-label">
+        <div className="mb-3">
+          <label htmlFor="confirm-password-input" className="form-label">
             Confirm Password
           </label>
-          <input type="password" class="form-control" id="confirm-" />
+          <input type="password" className="form-control" id="confirm-" />
         </div>
-        <div class="d-grid gap-2">
-          <button type="submit" class="btn btn-primary mt-3 mb-3">
+        <div className="d-grid gap-2">
+          <button type="submit" className="btn btn-primary mt-3 mb-3">
             REGISTER
           </button>
-          <button type="button" class="btn btn-dark">
+          <button type="button" className="btn btn-dark">
             Aready have an account? Login
           </button>
         </div>
@@ -43,3 +45,5 @@ export default function Register() {
     </div>
   );
 }
+
+
