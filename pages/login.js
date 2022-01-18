59abc31b754221054a108ca1d7413a5dfe24a785
Doni Abeje <doni@peoplise.com>
2022-01-18 17:46:19 +0300

    design login page

---
+++ pages/login.js
@@ -0,0 +1,39 @@
+export default function Login() {
+  return (
+    <div
+      style={{ height: "100vh", width: "100%" }}
+      className="d-flex justify-content-center align-items-center"
+    >
+      <form className="bg-light p-5 shadow-sm">
+        <div class="mb-3" style={{ width: "30rem" }}>
+          <h1 className="mb-5 text-center"><span className="text-primary">FILMS</span> | LOGIN</h1>
+          <label for="username-input" class="form-label">
+            Username
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="username-input"
+            placeholder=""
+          />
+        </div>
+        <div class="mb-3">
+          <label for="password-input" class="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="password-input"
+            placeholder="John Doe"
+          />
+        </div>
+        <div class="d-grid gap-2">
+          <button type="submit" class="btn btn-primary mt-3 mb-3">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
