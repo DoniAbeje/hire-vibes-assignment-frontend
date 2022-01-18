edd7ba30a0029b3e157ee1997848e8b64df32d11
Doni Abeje <doni@peoplise.com>
2022-01-19 00:14:39 +0300

    change navbar display for authenticated and unauthenticated user

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { isAuthenticated } from "../service";
 
 export default function Navbar() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+     setAuthenticated(isAuthenticated());
+  })
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
       <div className="container">
@@ -20,21 +27,27 @@ export default function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
-            <li className="nav-item">
-              <span className="nav-link ">
-                <Link href="/films/create">Add New</Link>
-              </span>
-            </li>
-            <li className="nav-item">
-              <span className="nav-link ">
-                <Link href="/register">Register</Link>
-              </span>
-            </li>
-            <li className="nav-item">
-              <span className="nav-link ">
-                <Link href="/login">Login</Link>
-              </span>
-            </li>
+            {authenticated && (
+              <li className="nav-item">
+                <span className="nav-link ">
+                  <Link href="/films/create">Add New</Link>
+                </span>
+              </li>
+            )}
+            {!authenticated && (
+              <li className="nav-item">
+                <span className="nav-link ">
+                  <Link href="/register">Register</Link>
+                </span>
+              </li>
+            )}
+            {!authenticated && (
+              <li className="nav-item">
+                <span className="nav-link ">
+                  <Link href="/login">Login</Link>
+                </span>
+              </li>
+            )}
           </ul>
         </div>
       </div>
