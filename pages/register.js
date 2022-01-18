531f992f86a297b538cde5d1e991862bb16a4ada
Doni Abeje <doni@peoplise.com>
2022-01-18 17:58:44 +0300

    design register page

---
+++ pages/register.js
@@ -0,0 +1,45 @@
+export default function Register() {
+  return (
+    <div
+      style={{ height: "100vh", width: "100%" }}
+      className="d-flex justify-content-center align-items-center"
+    >
+      <form className="bg-light p-5 shadow-sm">
+        <div class="mb-3" style={{ width: "30rem" }}>
+          <h1 className="mb-5 text-center">
+            <span className="text-primary">FILMS</span> | Register
+          </h1>
+          <label for="username-input" class="form-label">
+            Username
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="username-input"
+            placeholder=""
+          />
+        </div>
+        <div class="mb-3">
+          <label for="password-input" class="form-label">
+            Password
+          </label>
+          <input type="password" class="form-control" id="password-input" />
+        </div>
+        <div class="mb-3">
+          <label for="confirm-password-input" class="form-label">
+            Confirm Password
+          </label>
+          <input type="password" class="form-control" id="confirm-" />
+        </div>
+        <div class="d-grid gap-2">
+          <button type="submit" class="btn btn-primary mt-3 mb-3">
+            REGISTER
+          </button>
+          <button type="button" class="btn btn-dark">
+            Aready have an account? Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
