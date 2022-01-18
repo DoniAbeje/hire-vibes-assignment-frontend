43908f8accd005c008d2bef3ef12d50008cc8d09
Doni Abeje <doni@peoplise.com>
2022-01-18 16:43:43 +0300

    add sample data

---
+++ data.js
@@ -0,0 +1,15 @@
+export const sampleFilm = {
+  name: "The Shawshank Redemption (1994)",
+  description:
+    "Andy Dufresne, a successful banker, is arrested for the murders of his wife and her lover, and is sentenced to life imprisonment at the Shawshank prison. He becomes the most unconventional prisoner.",
+  releaseDate: new Date("Sep 17 1994").toISOString(),
+  rating: 4,
+  ticketPrice: 10.99,
+  country: "USA",
+  genre: Array("Drama"),
+  photo:
+    "https://images-na.ssl-images-amazon.com/images/P/B000P0J0EW.01._SX200_SCLZZZZZZZ_.jpg",
+  slug: "the-shawshank-redemption-1994",
+};
+
+export const sampleFilms = [1, 2, 3, 4].map((e) => sampleFilm);
